1e3591a3aaa9c77415aee6132b5ec68cef0e1b9f
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-21 13:04:48 -0500

    Haciendo retos de la sesion 2

--- Sesion-2/app.js
+++ Sesion-2/app.js
@@ -1,4 +1,18 @@
 // Reto 1
+const time = 24;
+let greeting
+if (time < 12) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good afternoon";
+} else if (time >= 20 && time <= 24) {
+  greeting = "Good evening";
+} else if (time > 24){
+  greeting = " ";
+}
+
+console.log(greeting) // Good afternoon
+
 
 
 
@@ -9,17 +23,17 @@
 //   }
 // }
 
-// Reto 3 Imprimiento numero primos
 
-for (let num = 1; num <= 100; num++) {
-  let vecesDivisible = 0;
-  for(let j = 1; j <= num; j++){
-    if ( num % j == 0){
-      vecesDivisible = vecesDivisible + 1;
-    }
-  }
-  if (vecesDivisible == 2) {
-    console.log(num)
-  }
-  vecesDivisible = 0;
-}
+// Reto 3 Imprimiento numero primos
+// for (let num = 1; num <= 100; num++) {
+//   let vecesDivisible = 0;
+//   for(let j = 1; j <= num; j++){
+//     if ( num % j == 0){
+//       vecesDivisible = vecesDivisible + 1;
+//     }
+//   }
+//   if (vecesDivisible == 2) {
+//     console.log(num)
+//   }
+//   vecesDivisible = 0;
+// }
