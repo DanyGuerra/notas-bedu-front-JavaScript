828e87f9301d67543f5e29af0298d27c39476204
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-21 12:46:49 -0500

    Completando el reto de funcion de numeros primos

--- Sesion-2/app.js
+++ Sesion-2/app.js
@@ -3,10 +3,23 @@
 
 
 // Reto 2 Imprimir numeros pares de 0 al 100
-for(var i = 0; i<=100; i++){
-  if ( i % 2 === 0){
-    console.log(i)
-  }
-}
+// for(var i = 0; i<=100; i++){
+//   if ( i % 2 === 0){
+//     console.log(i)
+//   }
+// }
 
-// Reto 3
+// Reto 3 Imprimiento numero primos
+
+for (let num = 1; num <= 100; num++) {
+  let vecesDivisible = 0;
+  for(let j = 1; j <= num; j++){
+    if ( num % j == 0){
+      vecesDivisible = vecesDivisible + 1;
+    }
+  }
+  if (vecesDivisible == 2) {
+    console.log(num)
+  }
+  vecesDivisible = 0;
+}
