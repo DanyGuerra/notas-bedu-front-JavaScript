d670bd39784cd67109e68269f55e04d22c6f0089
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-22 13:33:42 -0500

    Agregando notas de la sesion 3

--- Sesion-3/app.js
+++ Sesion-3/app.js
@@ -2,9 +2,18 @@
 function power(base, exponent){
   let acumulado = 1;
   for(let i=0;i<exponent;i++){
-    acumulado = acumulado * base;
+    acumulado *= base;
   }
   return acumulado;
 }
+console.log('Funcion exponencial: ', power(5,2));
 
-console.log(power(5,2));
+// Reto 2 Numero mayor que
+function getLargerInt(number1, number2) {
+  if (number1 > number2) {
+    return number1
+  } else {
+    return number2
+  }
+}
+console.log('Funcion de numero mayor: ', getLargerInt(52, 22));
