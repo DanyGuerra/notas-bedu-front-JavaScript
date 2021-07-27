9b8b6237532de7987e22d2a248d6c31ec193f9c1
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-26 20:48:32 -0500

    Notas de la sesion 3

--- Sesion-3/app.js
+++ Sesion-3/app.js
@@ -1,36 +1,11 @@
-// Reto 1 Potencia de un numero
-function power(base, exponent){
-  let acumulado = 1;
-  for(let i=0;i<exponent;i++){
-    acumulado *= base;
-  }
-  return acumulado;
-}
-console.log('Funcion exponencial: ', power(5,2));
+// Reto 1 calcular promedio
+const numbers = [5, 3, 4, 7, 2, 1, 9, 7, 7];
+var numElementos = numbers.length;
+var promedio;
+var suma = 0;
 
-// Reto 2 Numero mayor que
-function getLargerInt(number1, number2) {
-  if (number1 > number2) {
-    return number1
-  } else {
-    return number2
-  }
+for (let i = 0; i < numElementos; i++){
+  suma += numbers[i];
 }
-console.log('Funcion de numero mayor: ', getLargerInt(52, 22));
-
-//Reto 3 Serie de Fibonacci
-function fibonacci(limit){
-  let serFibonacci = [];
-  if (limit < 1){
-    return console.log('Limite debe ser mayor a 0')
-  }
-  for(let i = 0; i < limit; i++){
-    if(i<=1){
-      serFibonacci.push(1);
-    }else{
-      serFibonacci.push(serFibonacci[i - 1] + serFibonacci[i - 2])
-    }
-  }
-  return serFibonacci;
-}
-console.log('Serie de fibonacci: ', fibonacci(15));
+promedio = suma / numElementos;
+console.log('El promedio es: ', promedio);
---
+++ Sesion-4/app.js
@@ -0,0 +1,50 @@
+
+// Reto 1 Potencia de un numero
+// function power(base, exponent){
+//   let acumulado = 1;
+//   for(let i=0;i<exponent;i++){
+//     acumulado *= base;
+//   }
+//   return acumulado;
+// }
+// console.log('Funcion exponencial: ', power(5,2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Reto 2 Numero mayor que
+// function getLargerInt(number1, number2) {
+//   if (number1 > number2) {
+//     return number1
+//   } else {
+//     return number2
+//   }
+// }
+// console.log('Funcion de numero mayor: ', getLargerInt(52, 22));
+
+//Reto 3 Serie de Fibonacci
+// function fibonacci(limit){
+//   let serFibonacci = [];
+//   if (limit < 1){
+//     return console.log('Limite debe ser mayor a 0')
+//   }
+//   for(let i = 0; i < limit; i++){
+//     if(i<=1){
+//       serFibonacci.push(1);
+//     }else{
+//       serFibonacci.push(serFibonacci[i - 1] + serFibonacci[i - 2])
+//     }
+//   }
+//   return serFibonacci;
+// }
+// console.log('Serie de fibonacci: ', fibonacci(15));
+
