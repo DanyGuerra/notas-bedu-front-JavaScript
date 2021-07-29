c19729d8ca68d6e061c3518820bca6eee9e69d7d
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-29 12:03:04 -0500

    Eliminando archivos html

--- Sesion-4/app.js
+++ Sesion-4/app.js
@@ -11,17 +11,21 @@
 
 
 
-// Reto 2 Numero mayor que
-const obtenerNumeroMayor = (numero1, numero2) => {
-  if (numero1 > numero2) {
-    return numero1
-  } else {
-    return numero2
-  }
-}
-console.log('El numero mayor es: ', obtenerNumeroMayor(52, 22));
+// Reto 2 Numero mayor que version1
+// const obtenerNumeroMayor = (numero1, numero2) => {
+//   if (numero1 > numero2) {
+//     return numero1
+//   } else {
+//     return numero2
+//   }
+// }
+// console.log('El numero mayor es: ', obtenerNumeroMayor(2, 22));
 
-
+// Reto 2 version2
+// const obtenerNumeroMayor = (numero1, numero2) => {
+//   return numero1 > numero2 ? numero1 : numero2;
+// }
+// console.log('El numero mayor es: ', obtenerNumeroMayor(222, 22));
 
 
 
@@ -41,4 +45,21 @@ console.log('El numero mayor es: ', obtenerNumeroMayor(52, 22));
 //   return serFibonacci;
 // }
 // console.log('Serie de fibonacci: ', fibonacci(20));
+
+//Reto 3 Serie de Fibonacci
+const fibonacci = (limit) => {
+  let serFibonacci = [];
+  for(let i = 0; i < limit; i++){
+    if(i <= 1){
+      serFibonacci.push(1);
+    }else{
+      serFibonacci.push(serFibonacci[i - 1] + serFibonacci[i - 2])
+    }
+  }
+  return serFibonacci;
+}
+console.log('Serie de fibonacci: ', fibonacci(20));
+
+
+
 
