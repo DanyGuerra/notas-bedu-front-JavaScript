ca976d97c74a003e639ff53932dac3043a94891e
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-07-28 21:33:48 -0500

    Retos de sesion 4

--- Sesion-4/app.js
+++ Sesion-4/app.js
@@ -1,38 +1,32 @@
 
 // Reto 1 Potencia de un numero
-function power(base, exponent){
-  let acumulado = 1;
-  for(let i=0;i<exponent;i++){
-    acumulado *= base;
-  }
-  return acumulado;
-}
-console.log('Funcion exponencial: ', power(5,2));
-
-
-
-
-
-
-
-
-
-
+// function power(base, exponent){
+//   let acumulado = 1;
+//   for(let i = 0; i < exponent; i++){
+//     acumulado *= base;
+//   }
+//   return acumulado;
+// }
+// console.log('Funcion exponencial: ', power(3,3));
 
 
 
 // Reto 2 Numero mayor que
-// function getLargerInt(number1, number2) {
-//   if (number1 > number2) {
-//     return number1
-//   } else {
-//     return number2
-//   }
-// }
-// console.log('Funcion de numero mayor: ', getLargerInt(52, 22));
+const obtenerNumeroMayor = (numero1, numero2) => {
+  if (numero1 > numero2) {
+    return numero1
+  } else {
+    return numero2
+  }
+}
+console.log('El numero mayor es: ', obtenerNumeroMayor(52, 22));
+
+
+
+
 
 //Reto 3 Serie de Fibonacci
-// function fibonacci(limit){
+// const fibonacci = (limit) => {
 //   let serFibonacci = [];
 //   if (limit < 1){
 //     return console.log('Limite debe ser mayor a 0')
@@ -46,5 +40,5 @@ console.log('Funcion exponencial: ', power(5,2));
 //   }
 //   return serFibonacci;
 // }
-// console.log('Serie de fibonacci: ', fibonacci(15));
+// console.log('Serie de fibonacci: ', fibonacci(20));
 
